944b4e0a0d3dd056a8dfb4089ccc9a0603ffd7c0
marc-antoine ourradour <ourradourm@gmail.com>
2024-04-29 21:21:31 +0200

    add multicall3 - testnet europa

--- lib/clients/wagmi/europaLiquidityHub.ts
+++ lib/clients/wagmi/europaLiquidityHub.ts
@@ -1,28 +1,30 @@
 import { defineChain } from "viem";
 
-
 export const europaLiquidityHub = /*#__PURE__*/ defineChain({
-  id: 1444673419,
-  name: 'Europa Liquidity Hub',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'sFUEL',
-    symbol: 'sFUEL',
-  },
-  rpcUrls: {
-    default: { http: ['https://testnet.skalenodes.com/v1/juicy-low-small-testnet'] },
-  },
-  blockExplorers: {
-    default: {
-      name: 'juicy-low-small-testnet.explorer.testnet.skalenodes',
-      url: 'https://juicy-low-small-testnet.explorer.testnet.skalenodes.com/',
-      apiUrl: 'https://juicy-low-small-testnet.explorer.testnet.skalenodes.com/api',
-    },
-  },
-  contracts: {
-    multicall3: {
-      address: '0xca11bde05977b3631167028862be2a173976ca11',
-      blockCreated: 1639079,
-    },
-  },
-})
+	id: 1444673419,
+	name: "Europa Liquidity Hub",
+	nativeCurrency: {
+		decimals: 18,
+		name: "sFUEL",
+		symbol: "sFUEL",
+	},
+	rpcUrls: {
+		default: {
+			http: ["https://testnet.skalenodes.com/v1/juicy-low-small-testnet"],
+		},
+	},
+	blockExplorers: {
+		default: {
+			name: "juicy-low-small-testnet.explorer.testnet.skalenodes",
+			url: "https://juicy-low-small-testnet.explorer.testnet.skalenodes.com/",
+			apiUrl:
+				"https://juicy-low-small-testnet.explorer.testnet.skalenodes.com/api",
+		},
+	},
+	contracts: {
+		multicall3: {
+			address: "0xD2C89700844C878b31093Fb87F83f2BB80438cFe",
+			blockCreated: 1270500,
+		},
+	},
+});
