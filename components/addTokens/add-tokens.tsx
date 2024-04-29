025052839d71741d91844c7130ca9c29dece5819
marc-antoine ourradour <ourradourm@gmail.com>
2024-04-29 20:45:27 +0200

    add faucet link europa testnet

--- components/addTokens/add-tokens.tsx
+++ components/addTokens/add-tokens.tsx
@@ -12,12 +12,14 @@ import { ArrowUpRight } from "lucide-react";
 import { useAccount, useChainId, useSwitchChain } from "wagmi";
 import TableTokens from "./table-tokens";
 import useWindow from "@/lib/hooks/useWindow";
+import { Button } from "../ui/button";
 
 const AddTokens = () => {
 	const { address, isConnected } = useAccount();
 	const { chains } = useSwitchChain();
 	const chainId = useChainId();
 	const windowWidthPx = useWindow();
+
 
 	const urlExplorer = useMemo(() => {
 		const chain = chains.find((chain) => chain.id === chainId);
@@ -37,18 +39,23 @@ const AddTokens = () => {
 	return (
 		<div className="py-10">
 			<Card>
-				<CardHeader>
-					<CardTitle>
-						ERC-20 - Add multiple tokens to your wallet in one click.
-					</CardTitle>
+				<div className="flex flex-col justify-between sm:flex-row">
+					<CardHeader>
+						<CardTitle>
+							ERC-20 - Add multiple tokens to your wallet in one click.
+						</CardTitle>
 
-					<CardDescription className="flex flex-row items-center gap-2 ">
-						{addressDisplayed}
-						<Link href={urlExplorer} target="_blank">
-							<ArrowUpRight />
-						</Link>
-					</CardDescription>
-				</CardHeader>
+						<CardDescription className="flex flex-row items-center gap-2 ">
+							{addressDisplayed}
+							<Link href={urlExplorer} target="_blank">
+								<ArrowUpRight />
+							</Link>
+						</CardDescription>
+					</CardHeader>
+					{chainId === 1444673419 && <Link href={"https://www.sfuelstation.com/"} target="_blank" className="m-6">
+						<Button variant={"outline"}>Faucet</Button>
+					</Link>}
+				</div>
 				<CardContent>
 					<TableTokens />
 				</CardContent>
